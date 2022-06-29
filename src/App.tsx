376509ecb2a18fb35f1e081dba9f33ad4ce75614
Kaden Hansen <kadendeanhansen@gmail.com>
2022-06-28 22:16:08 -0400

    configure scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './App.css'
+import './css/App.css'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>A Little Slice of Joy</h1> */}
+      <h1>A Little Slice of Joy</h1>
       <Navbar />
     </div>
   )
