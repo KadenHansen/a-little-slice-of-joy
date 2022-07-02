ea01d2c9054ded357f42355fc0cc4cd3a6e6ff6c
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-01 20:59:13 -0400

    add navbar and navbar scss styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import '../css/Navbar.css'
+
+const handleClick = (e: any) => {
+  const navbar: any | null = document.querySelector(".navbar")
+  e.preventDefault()
+  navbar.querySelector(".active").classList.remove("active")
+  e.target.closest(".icon-set").classList.add("active")
+}
+
+const logo = require('../images/sliceOfJoyLogoWords.png')
 
 export default function Navbar() {
     return (
-        <div className="navbar">
-            
-        </div>
+        <nav className="navbar">
+          <ul>
+            <li className="icon-set active" onClick={handleClick}>
+              <a href="#" >
+                <i className='bx bxs-home-heart icon'></i>
+                <p>Home</p>
+              </a>
+            </li>
+            <li className="icon-set" onClick={handleClick}>
+              <a href="#">
+                <i className='bx bxs-cart icon' ></i>
+                <p>Cart</p>
+              </a>
+            </li>
+            <li>
+              <img src={logo}/>
+            </li>
+            <li className="icon-set" onClick={handleClick}>
+              <a href="#">
+                <i className='bx bxs-cookie icon' ></i>
+                <p>Gallery</p>
+              </a>
+            </li>
+            <li className="icon-set" onClick={handleClick}>
+              <a href="#">
+                <i className='bx bxs-heart icon' ></i>
+                <p>Contact</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
     )
 }
