c29c0df0dc26721ada6b792cb89ab5404892222c
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-02 12:14:42 -0400

    add body section and footer placeholder bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './styles/css/App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+        <div className='container'>
+          
+        </div>
+      <Footer />
     </div>
   )
 }
