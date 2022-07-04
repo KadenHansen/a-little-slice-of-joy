9754cc4468c4d4fbb64eab5b3a19034959e6dddc
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-03 22:12:06 -0400

    configure react routing to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import './styles/css/App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Homepage from './components/Homepage/Homepage'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-        <div className='container'>
-          <Homepage />
-        </div>
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+            </Routes>
+          </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
