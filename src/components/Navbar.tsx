5d77c917e56b80e16b33a9a2b29ec4c5fbcf9dd2
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-03 22:31:24 -0400

    refactor navbar to react routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../styles/css/Navbar.css'
 
-const handleClick = (e: React.MouseEvent<HTMLLIElement>) => {
+const handleClick = (e: any) => {
   e.preventDefault()
-  const navbar = document.querySelector(".navbar") as HTMLElement
+  const navbar: any = document.querySelector(".navbar")
   navbar.querySelector(".active")!.classList.remove("active");
-  (e.target as HTMLElement).closest(".icon-set")!.classList.add("active")
+  e.target.closest(".icon-set")!.classList.add("active")
 }
 
 const logo = require('../styles/images/sliceOfJoyLogoWords.png')
@@ -15,31 +16,31 @@ export default function Navbar(): JSX.Element {
         <nav className="navbar">
           <ul>
             <li className="icon-set active" onClick={handleClick}>
-              <a href="#" >
+              <Link to="/" >
                 <i className='bx bxs-home-heart icon'></i>
                 <p>Home</p>
-              </a>
+              </Link>
             </li>
             <li className="icon-set" onClick={handleClick}>
-              <a href="#">
+              <Link to="/gallery">
                 <i className='bx bxs-cookie icon' ></i>
                 <p>Gallery</p>
-              </a>
+              </Link>
             </li>
             <li>
               <img src={logo}/>
             </li>
             <li className="icon-set" onClick={handleClick}>
-              <a href="#">
+              <Link to="/contact">
                 <i className='bx bxs-heart icon' ></i>
                 <p>Contact</p>
-              </a>
+              </Link>
             </li>
             <li className="icon-set" onClick={handleClick}>
-              <a href="#">
+              <Link to="/cart">
                 <i className='bx bxs-cart icon' ></i>
                 <p>Cart</p>
-              </a>
+              </Link>
             </li>            
           </ul>
         </nav>
