93b539847973621eb57df2e175a1aed140dea127
Kaden Hansen <kadendeanhansen@gmail.com>
2022-07-16 14:33:40 -0400

    add admin form to add services

--- src/components/EmployeePortal/AdminServices.tsx
+++ src/components/EmployeePortal/AdminServices.tsx
@@ -17,24 +17,40 @@ export default function AdminServices() {
     }
 
     //handle submit with fetch request
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        const res = await fetch('http://localhost:8000/services', {
+          method: 'POST',
+          body: JSON.stringify({
+            title: service.title,
+            image: service.image,
+            description: service.description
+          }),
+          headers: {
+            'content-type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+        })
+        
+      }
 
   return (
     <div className="services">
         <h2 className='header'>Services</h2>
         <div className="add-service">
             <h3>Add Service</h3>
-            <form action='/' method='POST'>
+            <form action='/' method='POST' onSubmit={handleSubmit}>
                 <div className="input-field">
                     <label htmlFor="title">Service:</label>
-                    <input type="text" id='title' name='title' />
+                    <input type="text" id='title' name='title' onChange={handleChange}/>
                 </div>
                 <div className="input-field">
                     <label htmlFor="image">Image:</label>
-                    <input type="url" id='image' name='image' />
+                    <input type="url" id='image' name='image' onChange={handleChange}/>
                 </div>
                 <div className="input-field">
                     <label htmlFor="description">Description:</label>
-                    <input type="text" id='description' name='description' />
+                    <input type="text" id='description' name='description' onChange={handleChange}/>
                 </div>
                 <div className="submit-btn">
                     <input type="submit" value="Submit"></input>
