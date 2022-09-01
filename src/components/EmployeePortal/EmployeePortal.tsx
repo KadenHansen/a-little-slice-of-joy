74e4e1349ed8881662ba7a9b753990097d614d51
Kaden Hansen <kadendeanhansen@gmail.com>
2022-09-01 01:05:19 -0400

    redirect employee portal page to login page if no user is detected

--- src/components/EmployeePortal/EmployeePortal.tsx
+++ src/components/EmployeePortal/EmployeePortal.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import AdminMenuItems from './AdminMenuItems/AdminMenuItems'
 import AdminServices from './AdminServices/AdminServices'
 import NewAdminUserForm from './NewAdminUserForm'
 
 export default function EmployeePortal() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!localStorage.currentUser) {
+      navigate("/admin")
+    }
+  })
   return (
     <div className="employee-portal">
       <AdminServices />
